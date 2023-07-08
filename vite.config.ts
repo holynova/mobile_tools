962625e3b288009923c96f2cbdf8de7875b9efbe
holynova <holy_nova@126.com>
2023-07-08 18:03:45 +0800

    feat: add eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import eslintPlugin from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // eslintPlugin(),
+    eslintPlugin({
+      cache: false,
+      include: [".jsx", ".tsx", ".ts"],
+      failOnError: false,
+      // include: /\.(jsx?|tsx?|vue|svelte)$/,
+    }),
+  ],
   base: "./",
   // publicDir: "./",
 });
