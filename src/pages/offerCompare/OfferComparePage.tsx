a24100ea035f380fb402d316329f70860fffd6b8
holynova <holy_nova@126.com>
2023-07-08 18:03:29 +0800

    feat: add offer compare page and requirement

---
+++ src/pages/offerCompare/OfferComparePage.tsx
@@ -0,0 +1,22 @@
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useMemo,
+} from "react";
+// import {} from "antd";
+// import './OfferComparePage.less'
+// import  {log} from ''
+interface Props {}
+
+const OfferComparePage: React.FC<Props> = (props) => {
+  // const [loading, setLoading] = useState(false)
+  return (
+    <div className="OfferComparePage">
+      <h3>OfferComparePage</h3>
+    </div>
+  );
+};
+
+export default OfferComparePage;
